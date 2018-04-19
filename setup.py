70c5e1462960e721e1267bd82a0b987718f96cb0
T Thiyagaraj <tstream.h@outlook.com>
2018-04-20 01:44:20 +0530

    Initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'javasaur',
+  packages = ['javasaur'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Pure python library for Java code analysis',
+  author = 'Thiyagaraj T',
+  author_email = 'tstream.h@outlook.com',
+  url = 'https://github.com/tstreamDOTh/javasaur', # use the URL to the github repo
+  download_url = 'https://github.com/tstreamDOTh/javasaur/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['java', 'javalang', 'javaparser', 'parser'], # arbitrary keywords
+  classifiers = [],
+)
